b73e39dc16c3c60fe41988f97b95be28e65b3971
jcole <ahlybabar1188@gmail.com>
2025-07-02 14:33:36 +0800

    Added A code for the script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function validateForm() {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  if (email === "" || password === "") {
+    alert("Please fill in all fields.");
+    return false;
+  }
+  window.location.href = "home.html";
+  return false;
+}
